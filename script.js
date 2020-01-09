a718a8e9995d5fcd49a23a6bbb8476461299cf43
Nabaraj Rai <nabaraj2055@gmail.com>
2020-01-09 22:10:58 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const usrName=Console.log(document.getElementById('username'));
+const emailAdd=console.log(document.getElementById('email'));
+if(usrName==''){
+    alert('you must write any username!');
+}
+else{
+    alert('success!');
+}
